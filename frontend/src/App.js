6019540056c9d00594d313de639f0517665228a7
Stephan <sgriesel@gmail.com>
2020-10-29 06:34:17 +0100

    bootstrap set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-
+import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-      <h1>Test</h1>
+      <Header></Header>
+      <main>
+        <Container>
+          <h1>Test</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
     </>
   );
 }
