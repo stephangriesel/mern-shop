387758c797d83bd3baf03416bb585f0e84d40ffb
Stephan <sgriesel@gmail.com>
2020-11-04 06:27:38 +0100

    colorize console

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import products from './data/products.js'
+import colors from 'colors'
 
 
 dotenv.config();
@@ -23,4 +24,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Running on port ${PORT} in ${process.env.NODE_ENV} mode`));
+app.listen(PORT, console.log(`Running on port ${PORT} in ${process.env.NODE_ENV} mode`.yellow.bold));
