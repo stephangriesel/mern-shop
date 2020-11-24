cbbf50aa8be5cbfe9f906003e33d1deb251e2625
Stephan <sgriesel@gmail.com>
2020-11-24 06:44:42 +0100

    login reducer& actions frontend

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,16 +6,27 @@ import { productListReducer, productDetailsReducer } from './reducers/productRed
 
 import { cartReducer } from './reducers/cartReducers'
 
+import { userLoginReducer } from './reducers/userReducers'
+
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage }
+    cart: {
+        cartItems: cartItemsFromStorage
+    },
+
+    userLogin: {
+        userInfo: userInfoFromStorage
+    }
 }
 
 const middleware = [thunk]
