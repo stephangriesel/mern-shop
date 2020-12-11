828289536b822ed110bbc624e3e59c6596d52083
Stephan <sgriesel@gmail.com>
2020-12-11 06:08:45 +0100

    add endpoint for payment

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -52,6 +52,35 @@ const getOrderById = asyncHandler(async (req, res) => {
     }
 })
 
+// @description     Update order to paid
+// @route           GET /api/orders/:id/pay
+// @access          Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            // Review for other payment gateways
+
+            // Paypal:
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+
+        const updatedOrder = await order.save()
+
+        res.json(updatedOrder)
+
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})
+
 export {
-    addOrderItems, getOrderById
+    addOrderItems, getOrderById, updateOrderToPaid
 }
