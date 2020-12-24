9a517ad89c6f27e329f0a15e9de67ee34eafabdd
Stephan <sgriesel@gmail.com>
2020-12-24 07:52:41 +0100

    product delete logic

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import {
+    productListReducer,
+    productDetailsReducer,
+    productDeleteReducer
+} from './reducers/productReducers'
 
 import { cartReducer } from './reducers/cartReducers'
 
@@ -26,6 +30,7 @@ import {
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
