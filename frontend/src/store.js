943c22bae4c9ecea071e0ef00c22defc4a738f30
Stephan <sgriesel@gmail.com>
2021-01-04 07:28:11 +0100

    update reviews logic frontend, buggy version, review

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,8 @@ import {
     productDetailsReducer,
     productDeleteReducer,
     productCreateReducer,
-    productUpdateReducer
+    productUpdateReducer,
+    productReviewCreateReducer
 } from './reducers/productReducers'
 
 import { cartReducer } from './reducers/cartReducers'
@@ -37,6 +38,7 @@ const reducer = combineReducers({
     productDelete: productDeleteReducer,
     productCreate: productCreateReducer,
     productUpdate: productUpdateReducer,
+    productReviewCreate: productReviewCreateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
