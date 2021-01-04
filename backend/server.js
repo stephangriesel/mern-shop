de7356ac6f7c2a87ceb6219326d64c908e486275
Stephan <sgriesel@gmail.com>
2021-01-04 06:07:48 +0100

    install morgan

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleWare.js'
 
 import productRoutes from './routes/productRoutes.js'
@@ -16,6 +17,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
