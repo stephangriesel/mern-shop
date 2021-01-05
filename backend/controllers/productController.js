ebb4c654e9f23bd622fb6b6657c2fb738af173c9
Stephan <sgriesel@gmail.com>
2021-01-05 19:52:58 +0100

    fix typo

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -111,7 +111,7 @@ const createProductReview = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
     if (product) {
-        const alreadyReviewed = product.reviews.find(review => review.user.toString() == req.user._id.toString())
+        const alreadyReviewed = product.reviews.find(review => review.user.toString() === req.user._id.toString())
 
         if (alreadyReviewed) {
             res.status(400)
