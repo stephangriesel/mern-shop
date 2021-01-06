78abe62c3b0b440ce8d47824454996c55bfba218
Stephan <sgriesel@gmail.com>
2021-01-06 06:41:04 +0100

    disable reviews

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -112,7 +112,7 @@ const ProductScreen = ({ history, match }) => {
                             </Card>
                         </Col>
                     </Row>
-                    <Row>
+                    {/* <Row>
                         <Col md={6}>
                             <h2>Reviews</h2>
                             {product.reviews.length === 0 && <Message>No Reviews</Message>}
@@ -161,7 +161,7 @@ const ProductScreen = ({ history, match }) => {
                                 </ListGroup.Item>
                             </ListGroup>
                         </Col>
-                    </Row>
+                    </Row> */}
                 </>
             )}
 
