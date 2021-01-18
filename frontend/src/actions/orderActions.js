f158712c8818bfc9b3ca06ae2aa26a31997729e5
Stephan <sgriesel@gmail.com>
2021-01-18 07:54:57 +0100

    clear basket on checkout

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+import { CART_CLEAR_ITEMS } from '../constants/cartConstants'
+
 import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
@@ -42,6 +44,11 @@ export const createOrder = (order) => async (dispatch, getState) => {
             type: ORDER_CREATE_SUCCESS,
             payload: data
         })
+        dispatch({
+            type: CART_CLEAR_ITEMS,
+            payload: data,
+        })
+        localStorage.removeItem('cartItems')
 
     } catch (error) {
         dispatch({
