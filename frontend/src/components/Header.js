8812878659f710eff5d87475159e645302d366ba
Stephan <sgriesel@gmail.com>
2021-01-19 07:07:33 +0100

    logout logic, remove data

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import SearchBox from './SearchBox'
 import { logout } from '../actions/userActions'
+import { USER_DETAILS_RESET } from '../constants/userConstants'
 
 const Header = () => {
 
@@ -14,6 +15,7 @@ const Header = () => {
 
     const logoutHandler = () => {
         dispatch(logout())
+        dispatch({ type: USER_DETAILS_RESET })
     }
 
     return (
