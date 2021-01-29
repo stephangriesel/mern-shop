834da99b2fe830ef9413ddc927781d70d75d31c6
Stephan <sgriesel@gmail.com>
2021-01-29 07:06:33 +0100

    test nodemailer WIP

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import path from 'path'
 
 import express from 'express'
+import mailpkg from 'nodemailer'
+
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
@@ -18,9 +20,20 @@ connectDB();
 
 const app = express();
 
+const nodemailer = mailpkg;
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "d690709feaa20c",
+        pass: "becd9163884d24"
+    }
+});
 
 app.use(express.json())
 
@@ -28,6 +41,21 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
+
+app.get("/mail", async (req, res) => {
+    const mailInfo = {
+        from: '"John Doe 😎" <johndoe132@example.com>',
+        to: "testemail333@example.com",
+        subject: "Test email",
+        text: "Sending test email 123456",
+    }
+    try {
+        await transport.sendMail(mailInfo)
+        res.send("email sent")
+    } catch (e) {
+        res.status(500).send("Something broke!")
+    }
+})
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 app.get('/api/config/contentfultoken', (req, res) => res.send(process.env.CONTENTFUL_TOKEN))
