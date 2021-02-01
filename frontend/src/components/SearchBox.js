dbc6ce38b117de5ba5f39ca402c55cd565b43230
Stephan <sgriesel@gmail.com>
2021-02-01 06:16:16 +0100

    search box styles update

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -15,7 +15,7 @@ const SearchBox = ({ history }) => {
     return (
         <Form onSubmit={submitHandler} inline>
             <Form.Control type='text' name='q' onChange={(e) => setKeyword(e.target.value)} placeholder='Search Products' className="mr-sm-2 ml-sm-5"></Form.Control>
-            <Button type="submit" variant="outline-success" className="p-2">Search</Button>
+            <Button type="submit" variant="outline-success" className="p-2"><i className="fas fa-search"></i></Button>
         </Form>
     )
 }
