6d832c72da4c6217e0cbcd8ffa95b0252f7d5b4b
Stephan <sgriesel@gmail.com>
2021-02-10 06:14:56 +0100

    style checkout steps

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -8,7 +8,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
             <Nav.Item>
                 {step1 ? (
                     <LinkContainer to='/login'>
-                        <Nav.Link>Sign In</Nav.Link>
+                        <Nav.Link className="font-weight-bold">> Sign In</Nav.Link>
                     </LinkContainer>
                 ) : (<Nav.Link disabled>Sign In</Nav.Link>
                     )}
@@ -16,7 +16,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
             <Nav.Item>
                 {step2 ? (
                     <LinkContainer to='/shipping'>
-                        <Nav.Link>Shipping</Nav.Link>
+                        <Nav.Link className="font-weight-bold">> Shipping</Nav.Link>
                     </LinkContainer>
                 ) : (<Nav.Link disabled>Shipping</Nav.Link>
                     )}
@@ -24,7 +24,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
             <Nav.Item>
                 {step3 ? (
                     <LinkContainer to='/payment'>
-                        <Nav.Link>Payment</Nav.Link>
+                        <Nav.Link className="font-weight-bold">> Payment</Nav.Link>
                     </LinkContainer>
                 ) : (<Nav.Link disabled>Payment</Nav.Link>
                     )}
@@ -32,7 +32,7 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
             <Nav.Item>
                 {step4 ? (
                     <LinkContainer to='/placeorder'>
-                        <Nav.Link>Place Order</Nav.Link>
+                        <Nav.Link className="font-weight-bold">> Place Order</Nav.Link>
                     </LinkContainer>
                 ) : (<Nav.Link disabled>Place Order</Nav.Link>
                     )}
